a3deeb8a4eba3a8287fd8e408a853477fd2c58ee
Tina Wulff <tina@tinawulff.dk>
2025-05-19 18:36:42 +0200

    header

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+
+import './Header.scss';
+import Logo from '../assets/rooster-logo.png';
+import LinkButton from './LinkButton';
+
+export default function Header() {
+
+
+    return (
+        <>
+        <img src={Logo} alt="Rooster logo" />
+
+        <nav>
+            <ul>
+                <li><a href="#">Menupunkt 1</a></li>
+                <li><a href="#">Menupunkt 2</a></li>
+                <li><a href="#">Menupunkt 3</a></li>
+                <li><a href="#">Menupunkt 4</a></li>
+                <li><a href="#">Menupunkt 5</a></li>
+            </ul>
+        </nav>
+
+        <LinkButton className="button__dark">
+        Book et møde
+        </LinkButton>
+        </>
+    )
+}
