5b1f2714a510963f4494653e18b591054e275437
Tina Wulff <tina@tinawulff.dk>
2025-05-22 15:40:19 +0200

    import file-names corrected

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 import './Styles/Style.scss';
 
-import Header from './components/header'
-import Hero from './components/hero'
-import TextSection from './components/TextSection'
-import ImageSection from './components/ImageSection';
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+import TextSection from './components/TextSection.jsx'
+import ImageSection from './components/ImageSection.jsx';
+import Footer from './components/Footer';
 
 import './components/ImageSection.scss';
 
 //Images:
 import SeaImage from './assets/splitsektion-1.jpg';
-import BirdImage from './assets//splitsektion-2.jpg';
+import BirdImage from './assets/splitsektion-2.jpg';
 import CategorySection from './components/CategorySection';
 import FormSection from './components/FormSection';
-import Footer from './components/footer';
+
 
 
 
