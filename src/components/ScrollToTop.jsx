adc894e030af0dd7686851759f48b0ffc9d0c3bb
Tina Wulff <tina@tinawulff.dk>
2025-09-26 12:48:19 +0200

    scrollvirker og menuhovereffekter

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,19 +1,17 @@
 
 
+
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
-
   useEffect(() => {
-    // Scroll både main og window (main først, hvis den findes)
     const main = document.querySelector('main');
-    if (main) {
-      main.scrollTo(0, 0);
-    }
+    if (main) main.scrollTo(0, 0);
     window.scrollTo(0, 0);
+    // Debug log
+    console.log('ScrollToTop triggered for', pathname);
   }, [pathname]);
-
   return null;
 }
